dcf77d24fda4256f9cdde8f5f5cf3b72d083756e
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-10-31 14:58:31 +0300

    Initial coomit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+
+    public FormDate birthday;
+    public String name;
+    public int[] passport = new int[10];
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
